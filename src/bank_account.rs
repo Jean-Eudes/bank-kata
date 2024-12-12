76c2efdfe6d3e981e61843c8ef2a4bd0e581d986
jean-eudes <jeudes7@gmail.com>
2024-12-12 22:09:36 +0100

    tiny improvment.

--- src/bank_account.rs
+++ src/bank_account.rs
@@ -1,31 +1,48 @@
 use chrono::{DateTime, Utc};
 use std::fmt::{Display, Formatter};
 use Transaction::{Deposit, Withdraw};
+use crate::bank_account::Error::AccountFundCanBePositive;
 
 type Amount = i64;
 
+#[derive(Debug)]
+pub enum Error {
+    AccountFundCanBePositive(Amount),
+}
 enum Transaction {
-    Deposit(DateTime<Utc>, Amount),
-    Withdraw(DateTime<Utc>, Amount),
+    Deposit(DateTime<Utc>, Amount, Amount),
+    Withdraw(DateTime<Utc>, Amount, Amount),
+}
+
+impl Transaction {
+    fn balance(&self) -> &Amount {
+        match self {
+            Deposit(_, _, balance) => balance,
+            Withdraw(_, _, balance) => balance,
+        }
+    }
+
 }
 
 impl Display for Transaction {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Deposit(date, amount) => {
+            Deposit(date, amount, balance) => {
                 write!(
                     f,
-                    "Deposit : '{}' {}",
-                    date.format("%Y-%m-%d %H:%M:%S"),
-                    amount
+                    "{} ||          || {:9} || {:7}",
+                    date.format("%Y/%m/%d"),
+                    amount,
+                    balance,
                 )
             }
-            Withdraw(date, amount) => {
+            Withdraw(date, amount, balance) => {
                 write!(
                     f,
-                    "Withdraw: '{}' {}",
-                    date.format("%Y-%m-%d %H:%M:%S"),
-                    amount
+                    "{} ||{:9} ||           || {:7}",
+                    date.format("%Y/%m/%d"),
+                    amount,
+                    balance,
                 )
             }
         }
@@ -39,39 +56,41 @@ pub struct BankAccount {
 }
 
 impl BankAccount {
-    pub fn create_new_account(account_number: String, initial_amount: Amount) -> Self {
-        Self {
+    pub fn create_new_account(
+        account_number: String,
+        initial_amount: Amount,
+    ) -> Result<Self, Error> {
+        if initial_amount < 0 {
+            return Err(AccountFundCanBePositive(initial_amount));
+        }
+        Ok(Self {
             initial_amount,
             account_number,
             transactions: vec![],
-        }
+        })
     }
 
     pub fn deposit(&mut self, amount: Amount) {
         let now = Utc::now();
-        self.transactions.push(Deposit(now, amount));
+        self.transactions
+            .push(Deposit(now, amount, self.balance() + amount));
     }
 
     pub fn withdraw(&mut self, amount: Amount) {
         let now = Utc::now();
-        self.transactions.push(Withdraw(now, amount));
+        self.transactions
+            .push(Withdraw(now, amount, self.balance() - amount));
     }
 
-    pub fn balance(&self) -> Amount {
-        let mut balance = self.initial_amount;
-        for transaction in &self.transactions {
-            balance += match transaction {
-                Deposit(_, amount) => *amount,
-                Withdraw(_, amount) => -amount,
-            }
-        }
-        balance
+    pub fn balance(&self) -> &Amount {
+        self.transactions.last()
+            .map_or(&self.initial_amount, |t| t.balance())
     }
 }
 
 impl Display for BankAccount {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "Account: {} {}", self.account_number, self.balance())?;
+        writeln!(f, " Date       || credit   || debit     || balance")?;
         for transaction in &self.transactions {
             writeln!(f, " {}", transaction)?;
         }
@@ -86,7 +105,7 @@ mod tests {
     #[test]
     fn should_create_new_account() {
         // Given / When
-        let bank_account = BankAccount::create_new_account("account_number".to_string(), 100);
+        let bank_account = BankAccount::create_new_account("account_number".to_string(), 100).unwrap();
 
         // Then
         assert_eq!(bank_account.initial_amount, 100);
@@ -96,46 +115,46 @@ mod tests {
     #[test]
     fn should_deposit_in_bank_account() {
         // Given
-        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100);
+        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100).unwrap();
 
         // When
         bank_account.deposit(50);
 
         // Then
         assert_eq!(bank_account.transactions.len(), 1);
-        assert!(matches!(bank_account.transactions[0], Deposit(_, 50)));
+        assert!(matches!(bank_account.transactions[0], Deposit(_, 50, 150)));
     }
 
     #[test]
     fn should_withdraw_in_bank_account() {
         // Given
-        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100);
+        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100).unwrap();
 
         // When
         bank_account.withdraw(50);
 
         // Then
         assert_eq!(bank_account.transactions.len(), 1);
-        assert!(matches!(bank_account.transactions[0], Withdraw(_, 50)));
+        assert!(matches!(bank_account.transactions[0], Withdraw(_, 50, 50)));
     }
 
     #[test]
     fn should_balance_in_bank_account() {
         // Given
-        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100);
+        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100).unwrap();
 
         // When
         bank_account.deposit(50);
         bank_account.withdraw(100);
 
         // Then
-        assert_eq!(bank_account.balance(), 50);
+        assert_eq!(bank_account.balance(), &50);
     }
 
     #[test]
     fn should_format_account() {
         // Given
-        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100);
+        let mut bank_account = BankAccount::create_new_account("account_number".to_string(), 100).unwrap();
 
         // When
         bank_account.deposit(50);
@@ -143,7 +162,10 @@ mod tests {
 
         // Then
         let re = Regex::new(
-            r"Account: account_number 50\n Deposit : '(.*)' 50\n Withdraw: '(.*)' 100\n",
+            r" Date       || credit   || debit     || balance
+                   .* ||          ||        50 ||     150
+                   .* ||      100 ||            ||      50
+",
         )
         .unwrap();
         assert!(re.is_match(&bank_account.to_string()));
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use crate::bank_account::BankAccount;
+use crate::bank_account::{BankAccount, Error};
 
 mod bank_account;
 
-fn main() {
-    let mut account = BankAccount::create_new_account("0987654323".to_string(), 2000);
+fn main() -> Result<(), Error> {
+    let mut account = BankAccount::create_new_account("0987654323".to_string(), 2000)?;
     account.deposit(100);
-    account.deposit(300);
-    account.withdraw(150);
+    account.deposit(3000);
+    account.withdraw(6000);
 
     println!("{account}");
+    Ok(())
 }
